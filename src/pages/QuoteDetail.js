54ecdc4e2f36c1e70e747a540ce5d6a0cc52b49c
RussellLin <superbr7789@gmail.com>
2022-03-10 08:17:36 +0800

    Add dynamic nested routes with template literal

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,6 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -7,6 +9,9 @@ const QuoteDetail = () => {
     <div>
       <h1>Quote Detail</h1>
       <h4>{params.quoteId}</h4>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   );
 };
