0f51df01c169497cb528f0a6a1e9b70fcc9afe37
RussellLin <superbr7789@gmail.com>
2022-03-13 14:05:19 +0800

    Add details like highlightedQuotes for quoteDetail component

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_DATA = [
+  { id: "q1", author: "Max", text: "Learning React is Great!!!" },
+  { id: "q2", author: "Russell", text: "Learning React is Great as well." },
+  { id: "q3", author: "Kurt", text: "Learning languages is so much fun!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found!</p>;
+  }
 
   return (
     <div>
-      <h1>Quote Detail</h1>
-      <h4>{params.quoteId}</h4>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
