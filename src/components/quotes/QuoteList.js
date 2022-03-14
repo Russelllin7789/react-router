84670aa210f4f7266bb40aa582bce1cf58864954
RussellLin <superbr7789@gmail.com>
2022-03-14 08:38:40 +0800

    Add dynamic sorting according to the queryParams within routes

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,53 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const sortQuotes = (quotes, ascending) => {
+    return quotes.sort((quoteA, quoteB) => {
+      if (ascending) {
+        return quoteA.id > quoteB.id ? 1 : -1;
+      } else {
+        return quoteA.id < quoteB.id ? 1 : -1;
+      }
+    });
+    // if (ascending) {
+    //   return quotes.sort((a, b) => {
+    //     console.log(a, b);
+    //     return a.id - b.id;
+    //   });
+    // } else {
+    //   return quotes.sort((a, b) => {
+    //     return b.id - a.id;
+    //   });
+    // }
+  };
+
+  // URLSearchParams is native JS method
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const sortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
